0cf3afe512ba0837ef3ad1659ba0d5b6fdd1d15d
AetheriXD <creatorofedits1234@gmail.com>
2026-05-26 21:04:04 +0600

    Create commandHandler.js

---
+++ src/handlers/commandHandler.js
@@ -0,0 +1,33 @@
+module.exports = (client) => {
+    const fs = require("fs");
+    const config = require("../config.json");
+
+    client.commands = new Map();
+
+    const folders = fs.readdirSync("./commands");
+
+    for (const folder of folders) {
+        const files = fs.readdirSync(`./commands/${folder}`)
+        .filter(f => f.endsWith(".js"));
+
+        for (const file of files) {
+            const cmd = require(`../commands/${folder}/${file}`);
+            client.commands.set(cmd.name, cmd);
+        }
+    }
+
+    client.on("messageCreate", (message) => {
+
+        if (message.author.bot) return;
+
+        if (!message.content.startsWith(config.prefix)) return;
+
+        const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
+        const command = client.commands.get(commandName);
+        if (!command) return;
+
+        command.execute(message, args, client);
+    });
+};
