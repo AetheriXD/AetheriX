52fed0c01027fc036300399889b045e01a50da78
AetheriXD <creatorofedits1234@gmail.com>
2026-05-28 17:05:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 import './src/app.js';
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
 
+  const prefix = "A!";
+
+  if (!message.content.startsWith(prefix)) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    message.reply("Pong!");
+  }
+});
